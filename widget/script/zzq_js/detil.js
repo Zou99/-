b0f418bad6ecf76fc82516909f2743916769ea79
Zzq99 <2426031873@qq.com>
2019-04-03 14:04:53 +0800

    详情

--- widget/script/zzq_js/detil.js
+++ widget/script/zzq_js/detil.js
@@ -18,8 +18,21 @@ $(document).ready(function() { //在文档加载完毕后执行
     })
 
     // 加入购物车
-    $(".aui-bg-danger").click(function(){
-      $(".zzq_spec_shop").show()
+    $(".aui-bg-danger,.aui-bg-warning").click(function(){
+      $(".zzq_spec_shop").show();
+      $("html,body").css({
+          'height': '100%',
+          'overflow': 'hidden'
+      });
+    })
+
+    // 关闭加入购物车的页面
+    $(".zzq_detail_danger_cuo2,.zzq_detail_spec_rgba2").click(function() {
+        $(".zzq_spec_shop").hide();
+        $("html,body").css({
+            'height': '',
+            'overflow': 'inherit'
+        });
     })
 
 })
